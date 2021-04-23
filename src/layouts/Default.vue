3fc6b9831f7c749fa34ac995d670c476eb4a0f12
Joe <josephwinger1@gmail.com>
2021-04-23 08:28:53 -0400

    Add search bar in header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -10,10 +10,30 @@
         </div>
         <nav class="nav-right">
           <!-- <g-link class="nav__link" to="/about/">About</g-link> -->
-          <a class="feedback" href="https://form.typeform.com/to/Dpti2OAs?typeform-medium=embed-snippet" target="_blank">Feedback</a>
+          <!-- <a class="feedback" href="https://form.typeform.com/to/Dpti2OAs?typeform-medium=embed-snippet" target="_blank">Feedback</a> -->
+          <input type="search" name="search" class="search" v-model="query">
+          <div class="account"></div>
         </nav>
       </div>
     </header>
+    <div class="drawer-wrapper" @click="currentDrawer=null" v-if="currentDrawer!==null">
+      <div class="drawer results-drawer" v-if="currentDrawer==='searchresults'">
+        <ol v-if="results !== null" class="results">
+          <li v-for="result in results" :key="result.id" class="result">
+				    <g-link :to="`/disc/${result.node.slug}/`">
+              <span class="name">{{result.node.name}}</span>
+              <span class="manufacturer">{{result.node.manufacturer.name}}</span>
+              <ol class="numbers">
+                <div class="number">{{result.node.speed}}</div>
+                <div class="number">{{result.node.glide}}</div>
+                <div class="number">{{result.node.turn}}</div>
+                <div class="number">{{result.node.fade}}</div>
+              </ol>
+            </g-link>
+          </li>
+        </ol>
+      </div>
+    </div>
     <div class="content">
       <slot />
     </div>
@@ -30,8 +50,49 @@ query {
   metadata {
     siteName
   }
+	discs: allDisc {
+		edges {
+			node {
+				id
+				name
+				slug
+        manufacturer {
+          name
+        }
+        speed
+        glide
+        turn
+        fade
+			}
+		}
+	}
 }
 </static-query>
+
+<script>
+export default {
+  data: function() {
+    return {
+      query: '',
+      currentDrawer: null
+    }
+  },
+  computed: {
+    results: function() {
+      if(this.query === '') return;
+      else this.currentDrawer = 'searchresults';
+
+      return this.$static.discs.edges.filter(disc => !disc.node.name.toLowerCase().indexOf(this.query.toLowerCase()));
+    }
+  },
+  watch: {
+    query: function(val) {
+      if (val !== null) this.currentDrawer = 'searchresults'
+      else if (val === '') this.currentDrawer = null;
+    }
+  }
+}
+</script>
 
 <style>
 
@@ -51,7 +112,7 @@ query {
   position: fixed;
   z-index: 100;
 
-  background: var(--bg-color);
+  background: var(--header-bg-color);
 
   width: 100%;
   box-sizing: border-box;
@@ -110,9 +171,124 @@ query {
   box-shadow: 0 0 2px var(--beta-chip-color),
         inset 0 0 2px var(--beta-chip-color);
   
-  margin-left: 10px;
+  margin-left: 5px;
 }
 
+.search {
+  width: 32px;
+  height: 32px;
+	box-sizing: border-box;
+	border: solid 1px rgba(56, 60, 79, 0.3);
+	border-radius: 10000px;
+
+  margin-right: 5px;
+
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	appearance: none;
+
+	font-size: 0.9rem;
+  transition: width 0.3s ease-out;
+  background-image: url('../../src/assets/search.svg');
+  background-size: 18px;
+  background-repeat: no-repeat;
+  background-position-x: 6px;
+  background-position-y: center;
+  color: transparent;
+}
+.search:focus {
+  width: calc(100% - 45px);
+  padding-left: 28px;
+  
+	outline: 0;
+  color: var(--text-color);
+	/* box-shadow: 0 0 0 1px rgba(56, 60, 79, 0.4); */
+}
+
+.account {
+  width: 32px;
+  height: 32px;
+	box-sizing: border-box;
+	border: solid 1px rgba(56, 60, 79, 0.3);
+	border-radius: 100%;
+
+  background-image: url('../../src/assets/user.svg');
+  background-size: 18px;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-position-y: center;
+  background-color: #EEE;
+}
+
+.drawer-wrapper {
+  top: var(--nav-height);
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: 99;
+  background: rgba(56, 60, 79, 0.7);
+}
+
+.drawer {
+  width: var(--content-max-width);
+  position: absolute;
+  padding-top: 15px;
+  background: var(--header-bg-color);
+  left: 0;
+  z-index: 99;
+  box-shadow: 0 1px rgba(0, 0, 0, 0.11);
+}
+.results-drawer {
+  height: var(--results-drawer-height);
+  overflow: scroll;
+
+  /* box-shadow: 0px 0px 1000px 1000px rgba(56, 60, 79, 0.8); */
+}
+
+.results-drawer .results {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.results-drawer .result {
+  width: 100vw;
+  height: var(--result-height);
+}
+.results-drawer .result a {
+  position: relative;
+  width: 100vw;
+  height: 100%;
+  padding: 0 30px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  
+  color: var(--text-color);
+  text-decoration: none;
+}
+.results-drawer .result a .name {
+  font-weight: bold;
+  margin-right: 8px;
+}
+.results-drawer .result a .manufacturer {
+  font-size: 0.8em;
+  font-style: italic;
+}
+.results-drawer .result a .numbers {
+  position: absolute;
+  right: 30px;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 120px;
+}
 .feedback {
   color: var(--text-color);
   border-bottom: dotted 1px var(--text-color);
