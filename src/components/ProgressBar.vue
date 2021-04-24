2608117eec29b74b1a5cf8339e5739b04888a56f
Joe <josephwinger1@gmail.com>
2021-04-24 18:23:59 -0400

    Fix progess bar value text falling off the left edge

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class='bar' :style="{ 'background-color':  backgroundColor }">
-		<div class='value' :style="{ 'background-color': foregroundColor, 'width': `${width}%` }">{{ value }}{{ suffix || '' }}</div>
+		<div class='value' ref='value' :style="{ 'background-color': foregroundColor, 'width': `${width}%` }"><span ref='valuetext' :class="{'valuetext': true, 'tinybar': this.tinybar}">{{ value }}{{ suffix || '' }}</span></div>
 	</div>
 </template>
 
@@ -15,13 +15,23 @@ export default {
 		backgroundColor: String,
 		foregroundColor: String
 	},
+	data() {
+		return {
+			tinybar: false
+		}
+	},
 	methods: {
 		mapScale: function(num, in_min, in_max, out_min, out_max) {
   			return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 		}
 	},
 	computed: {
-		width() { return this.mapScale(this.value, this.min, this.max, 0, 100) }
+		width() { return this.mapScale(this.value, this.min, this.max, 0, 100) },
+	},
+	mounted() {
+    	this.$nextTick(() => {
+        	this.tinybar = this.$refs.valuetext.clientWidth*1.3 > this.$refs.value.clientWidth;
+    	});
 	}
 }
 </script>
@@ -45,6 +55,8 @@ export default {
 	height: calc(var(--bar-height) - calc(var(--bar-gutter) * 2));
 	min-width: calc(var(--bar-height) - calc(var(--bar-gutter) * 2)) !important;
 
+	position: relative;
+
 	font-weight: 500;
 	box-sizing: border-box;
 	padding-right: 5px;
@@ -56,5 +68,10 @@ export default {
 	justify-content: flex-end;
 	align-items: center;
 
+}
+.valuetext.tinybar {
+	position: absolute;
+	left: 5px;
+	border: solid salmon 1px;
 }
 </style>
